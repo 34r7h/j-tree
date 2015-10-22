e0765d14171789dee3640dbd3dd84bf60f60dd99
irthos <irth03@gmail.com>
2015-10-21 22:00:11 -0700

    range slider better

--- app/scripts/global-services/api-f.js
+++ app/scripts/global-services/api-f.js
@@ -30,6 +30,17 @@ angular.module('jtree')
         };
 
         service.style = {
+            rangeTrack: function(percent){
+                return {
+                    /*'background': '-moz-linear-gradient(90deg,  rgba(0,0,0,.6) 0%, rgba(0,0,0,.5) '+ percent + '%' +', rgba(0,0,0,.4) '+ percent + '%' +', rgba(0,0,0,.2) '+ percent + '%' +', rgba(0,0,0,.4) '+ percent + '%' +', rgba(0,0,0,.3) 100%)',
+                    background: '-webkit-gradient(linear, left bottom, right top, color-stop(0%,rgba(0,0,0,.6)), color-stop('+ percent + '%' +',rgba(0,0,0,.5)), color-stop('+ percent + '%' +',rgba(0,0,0,.4)), color-stop('+ percent + '%' +',rgba(0,0,0,.2)), color-stop('+ percent + '%' +',rgba(0,0,0,.4)), color-stop(100%,rgba(0,0,0,.3)))',
+                    background: '-webkit-linear-gradient(90deg,  rgba(0,0,0,.6) 0%,rgba(0,0,0,.5) '+ percent + '%' +',rgba(0,0,0,.4) '+ percent + '%' +',rgba(0,0,0,.2) '+ percent + '%' +',rgba(0,0,0,.4) '+ percent + '%' +',rgba(0,0,0,.3) 100%)',
+                    background: '-o-linear-gradient(90deg,  rgba(0,0,0,.6) 0%,rgba(0,0,0,.5) '+ percent + '%' +',rgba(0,0,0,.4) '+ percent + '%' +',rgba(0,0,0,.2) '+ percent + '%' +',rgba(0,0,0,.4) '+ percent + '%' +',rgba(0,0,0,.3) 100%)',
+                    background: '-ms-linear-gradient(90deg,  rgba(0,0,0,.6) 0%,rgba(0,0,0,.5) '+ percent + '%' +',rgba(0,0,0,.4) '+ percent + '%' +',rgba(0,0,0,.2) '+ percent + '%' +',rgba(0,0,0,.4) '+ percent + '%' +',rgba(0,0,0,.3) 100%)',*/
+                    background: 'linear-gradient(90deg,  rgba(0,0,0,.6) 0%,rgba(0,0,0,.5) '+ percent + '%' +',rgba(0,0,0,.4) '+ percent + '%' +',rgba(0,0,0,.2) '+ percent + '%' +',rgba(0,0,0,.4) '+ percent + '%' +',rgba(0,0,0,.1) 100%)',
+                    filter: 'progid:DXImageTransform.Microsoft.gradient( startColorstr=\'rgba(0,0,0,.6)\', endColorstr=\'rgba(0,0,0,.3)\',GradientType=1 )'
+                }
+            },
             forward: function(){
                 service.style.motion.slide='slide-right';
             },
